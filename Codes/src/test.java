27355523dd53bfd855d56dc597b128d4bf8e166a
Yoshiki Tanaka <tanaka@sakailab.info>
2015-11-13 17:08:28 +0900

    プログラムの基本helloworldを表示するプログラム

--- Codes/src/test.java
+++ Codes/src/test.java
@@ -1,5 +1,12 @@
-
 public class test {
+	static final int[] HELLO_WORLD = { 72, 29, 7, 0, 3, -79, 87, -8, 3, -6, -8,
+			-67 };
 
-	
+	public static void main(String[] args) {
+		int buf = 0;
+		for (int hw : HELLO_WORLD) {
+			System.out.print((char) buf);
+			buf += hw;
+
+	}
 }
