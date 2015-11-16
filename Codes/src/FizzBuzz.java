75311835e92d9120738f3f931fadf4ccd794f742
Yoshiki Tanaka <tanaka@sakailab.info>
2015-11-16 17:25:34 +0900

    FizzBuzz問題を解決

---
+++ Codes/src/FizzBuzz.java
@@ -0,0 +1,26 @@
+
+public class FizzBuzz {
+	static final Object FIZZ_BUSS = fizbuz(30, 1);
+
+	static Object fizbuz(int max, int num) {
+
+		System.out.println(num % 3 == 0 && num %5 != 0	? "fizz" :
+						   num % 3 != 0 && num % 5 == 0	? "buzz" :
+						   num % 3 == 0 && num % 5 == 0	? "fizz buzz" : 
+					       num);
+
+		System.exit(num < max ? (int)fizbuz(max, ++num) : 0);
+		return null;
+	}
+
+	public static void main(String[] args) {
+		\u002f\u002a
+		Object null = null;
+		Object null = null;
+		if (null == null ^ null == null) {
+			throw null;
+		}
+        \u002a\u002f
+	}
+
+}
