016c4446ea6661af123d3e994886d4888301377e
erxca <suzuki@sakailab.info>
2015-12-02 13:13:58 +0900

    値を追加

--- Codes/src/test.java
+++ Codes/src/test.java
@@ -1,5 +1,5 @@
 public class test {
-	static final int[] HELLO_WORLD = { 72, 29, 7, 0, 3, -79, 87, -8, 3, -6, -8, -67 };
+	static final int[] HELLO_WORLD = { 72, 29, 7, 0, 3, -79, 87, -8, 3, -6, -8, -67, -100 };
 
 	public static void main(String[] args) {
 		int buf = 0;
